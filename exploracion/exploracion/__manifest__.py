8729fe860a442518aeb91796f47c1d9268af137d
Walter Bustillo <walter.bustillo@alphasys.com.bo>
2022-01-18 16:06:18 +0000

    [ADD] Modificaciones menores

--- exploracion/exploracion/__manifest__.py
+++ exploracion/exploracion/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Exploracion',
-    'version': '0.4.1',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase', 'contacts'],
